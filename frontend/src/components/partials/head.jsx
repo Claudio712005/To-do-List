417cee2b82ada768f6f7dbb7288d0729ef9aa9e1
Claudio712005 <clausilvaaraujo11@gmail.com>
2024-01-24 11:06:47 -0300

    Adicioando uma barra para ocultar as tarefas ou exibir o formulário para criar uma nova tarefa.

--- frontend/src/components/partials/head.jsx
+++ frontend/src/components/partials/head.jsx
@@ -1,12 +1,26 @@
-import '../../style/head.sass'
+import React from 'react';
+import '../../style/head.sass';
+import { useLocation } from 'react-router-dom';
 
-function Head(){
-    return(
-        <div className="head">
-            <h1>CLÁUDIO ARAÚJO</h1>
-            <h2>TO do List project</h2>
-        </div>
-    )
+function Head({ toggleFormVisibility, formVisibility }) {
+  const location = useLocation();
+
+  if (location.pathname === '/') {
+    return (
+      <div className="head">
+        <h1>CLÁUDIO ARAÚJO</h1>
+        <h2>TO do List project</h2>
+      </div>
+    );
+  } else if (location.pathname === '/tasks') {
+    return (
+      <div onClick={toggleFormVisibility} className="head-task">
+        {formVisibility ? 'SHOW MY TASKS' : 'CREATE NEW TASK'}
+      </div>
+    );
+  }
+
+  return null;
 }
 
-export default Head
+export default Head;
--- frontend/src/components/taskPage.jsx
+++ frontend/src/components/taskPage.jsx
@@ -1,13 +1,21 @@
-import CreateTask from "./partials/createTask"
-import "../style/taskPage.sass"
+import React, { useState } from 'react';
+import '../style/taskPage.sass';
+import Head from './partials/head';
+import CreateTask from './partials/createTask';
 
-function TaskPage(){
+function TaskPage() {
+  const [formVisibility, setFormVisibility] = useState(true );
 
-    return(
-        <div className="contentTask">
-            <CreateTask />
-        </div>
-    )
+  const toggleFormVisibility = () => {
+    setFormVisibility(!formVisibility);
+  };
+
+  return (
+    <div className="contentTask">
+      <Head toggleFormVisibility={toggleFormVisibility} formVisibility={formVisibility} />
+      {formVisibility ? <CreateTask /> : null}
+    </div>
+  );
 }
 
-export default TaskPage
+export default TaskPage;
