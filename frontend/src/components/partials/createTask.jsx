e7b727b2818b544a424d69e29d1246ae5c220fc9
Claudio712005 <clausilvaaraujo11@gmail.com>
2024-01-24 10:32:55 -0300

    Criação do formulário de task

--- frontend/src/components/partials/createTask.jsx
+++ frontend/src/components/partials/createTask.jsx
@@ -1,7 +1,19 @@
+import "../../style/createTask.sass"
+
 function CreateTask(){
     return(
-        <div>
-            
+        <div className="containerCreator">
+            <h2>Create your task</h2>
+            <label htmlFor="" name="name">Task Name</label>
+            <input type="text" name="name"/>
+            <label htmlFor="" name="description">Task description</label>
+            <textarea name="description" id="" cols="30" rows="10"></textarea>
+            <label htmlFor="" name="Task date">Task date</label>
+            <input type="date" name="taskDate" id=""/>
+            <div className="button-container">
+                <button>CREATE</button>
+                <button>CANCEL</button>
+            </div>
         </div>
     )
 }
